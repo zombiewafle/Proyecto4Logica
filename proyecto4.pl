cce8f0b696d1d892a22c0232bde6af3998eab04b
JmDeleon2000 <54918136+JmDeleon2000@users.noreply.github.com>
2021-10-27 03:14:12 -0600

    parsing

--- proyecto4.pl
+++ proyecto4.pl
@@ -1,16 +1,76 @@
-% Proyecto 4 de la clase de Lógica Matemática 
-% Este programa debe calcular la derivada sumas, restas, multiplicaciones, divisiones
-% , funciones polinomiales, coseno, seno, tangente, arctan, exponentes y logaritmo natural
 
 
-% Derivada de una constante
-derivada(U,	0):- 
-	number(U).	
+asciiExp([pAbre(_), asciiExp(_), pCierra(_)]).
+%asciiExp([asciiExp(_), opSum(_), asciiExp(_)]).
+%asciiExp([asciiExp(_), opMul(_), asciiExp(_)]).
+%asciiExp([asciiExp(_), opDivision(_), asciiExp(_)]).
+%asciiExp([asciiExp(_), opResta(_), asciiExp(_)]).
 
-	
-% Derivada de x^n
+%https://stackoverflow.com/questions/32931732/convert-compound-to-atom-in-prolog
 
-derivada(U^N, X, N*X^V):- 
-	N > 0,
-	V is N-1.
+
+asciiExp(Ascii):-
+	numero(Ascii).
+
+%suma
+asciiExp(Ascii):-
+	[H|T] = Ascii,
+	numero(H),
+	[H2, T2] = T,
+	opSum(H2);
+	opMul(H2);
+	opResta(H2);
+	opDivision(H2);
+	opElevacion(H2);
+	asciiExp(T2).
+
+%asciiExp(Ascii):-
+%	[H|T] = Ascii,
+%	numero(H),
+%	[H2, T2] = T,
+%	opMul(H2),
+%	asciiExp(T2).
+%
+%asciiExp(Ascii):-
+%	[H|T] = Ascii,
+%	numero(H),
+%	[H2, T2] = T,
+%	opResta(H2),
+%	asciiExp(T2).
+
+expresion(X):-
+	name(X, Ascii),
+	asciiExp(Ascii).
+
+digito(Ascii) :-
+	Ascii >= 48,
+	Ascii =< 57.
+
+numero(Ascii):-
+	\+ is_list(Ascii),
+	digito(Ascii).
+
+%evaluacion recursiva
+numero(Ascii):-
+	[H|T] = Ascii,
+	digito(H),
+	numero(T).
+
+pAbre(Ascii):-
+	Ascii = 40.
+pCierra(Ascii):-
+	Ascii = 41.
+
+opMul(Ascii):-
+	Ascii = 42.
+opSum(Ascii):-
+	Ascii = 43.
+opResta(Ascii):-
+	Ascii = 45.
+opDivision(Ascii):-
+	Ascii = 47.
+
+opElevacion(Ascii):-
+	Ascii = 94.
+
 
