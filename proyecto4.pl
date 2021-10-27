55127ed339c46ce56d4748e5be757a888f313522
JmDeleon2000 <54918136+JmDeleon2000@users.noreply.github.com>
2021-10-27 14:58:29 -0600

    derivacion v0.7
    falta multiplicacion y divison

--- proyecto4.pl
+++ proyecto4.pl
@@ -8,13 +8,10 @@ asciiExp([pAbre(_), asciiExp(_), pCierra(_)]).
 %https://stackoverflow.com/questions/32931732/convert-compound-to-atom-in-prolog
 
 
-asciiExp(Ascii):-
-	numero(Ascii).
-
 %suma
 asciiExp(Ascii):-
 	[H|T] = Ascii,
-	numero(H),
+	asciiExp(H),
 	[H2, T2] = T,
 	opSum(H2),
 	asciiExp(T2).
@@ -22,7 +19,7 @@ asciiExp(Ascii):-
 %multiplicacion
 asciiExp(Ascii):-
 	[H|T] = Ascii,
-	numero(H),
+	asciiExp(H),
 	[H2, T2] = T,
 	opMul(H2),
 	asciiExp(T2).
@@ -30,7 +27,7 @@ asciiExp(Ascii):-
 %resta
 asciiExp(Ascii):-
 	[H|T] = Ascii,
-	numero(H),
+	asciiExp(H),
 	[H2, T2] = T,
 	opResta(H2),
 	asciiExp(T2).
@@ -38,7 +35,7 @@ asciiExp(Ascii):-
 %division
 asciiExp(Ascii):-
 	[H|T] = Ascii,
-	numero(H),
+	asciiExp(H),
 	[H2, T2] = T,
 	opDivision(H2),
 	asciiExp(T2).
@@ -46,10 +43,13 @@ asciiExp(Ascii):-
 %opElevacion
 asciiExp(Ascii):-
 	[H|T] = Ascii,
-	numero(H),
+	asciiExp(H),
 	[H2, T2] = T,
 	opElevacion(H2),
 	asciiExp(T2).
+
+asciiExp(Ascii):-
+	numero(Ascii).
 
 expresion(X):-
 	name(X, Ascii),
@@ -58,6 +58,9 @@ expresion(X):-
 digito(Ascii) :-
 	Ascii >= 48,
 	Ascii =< 57.
+
+digito(X):-
+	X = [].
 
 numero(Ascii):-
 	\+ is_list(Ascii),
@@ -68,6 +71,11 @@ numero(Ascii):-
 	[H|T] = Ascii,
 	digito(H),
 	numero(T).
+
+numero(X):-
+	X = [].
+
+
 
 pAbre(Ascii):-
 	Ascii = 40.
@@ -86,4 +94,57 @@ opDivision(Ascii):-
 opElevacion(Ascii):-
 	Ascii = 94.
 
+%https://gist.github.com/tokkenno/8038482
 
+
+%multiplicación y división de funciones polinomiales, 
+
+derivada(X, X, 1).
+
+derivada(X, _, Yields):-
+	atom(X),
+	name(X, Ascii),
+	numero(Ascii),
+	Yields = 0.
+
+derivada(X + Y, Var, Yields):-
+	derivada(X, Var, D1),
+	derivada(Y, Var, D2),
+	Yields =  D1 + D2.
+
+derivada(X - Y, Var, Yields):-
+	derivada(X, Var, D1),
+	derivada(Y, Var, D2),
+	Yields =  D1 + D2.
+
+derivada(ln(X), Var, Yields):-
+	derivada(X, Var, D1),
+	Yields = 1/X*D1.
+
+derivada(cos(X), Var, Yields):-
+	derivada(X, Var, D1),
+	Yields = -sen(X)*D1.
+
+derivada(sen(X), Var, Yields):-
+	derivada(X, Var, D1),
+	Yields = cos(X)*D1.
+
+derivada(sin(X), Var, Yields):-
+	derivada(X, Var, D1),
+	Yields = cos(X)*D1.
+
+derivada(arctan(X), Var, Yields):-
+	derivada(X, Var, D1),
+	Yields = 1/(X^2+1)*D1.
+
+derivada(tan(X), Var, Yields):-
+	derivada(X, Var, D1),
+	Yields = sec2(X)*D1.
+
+derivada(exp(X), Var, Yields):-
+	derivada(X, Var, D1),
+	Yields = exp(X)*D1.
+
+%derivada(X ^ Y, Var, Yields) :-
+%derivada(X*Y, Var, Yields):-
+%derivada(X/Y, Var, Yields):-
