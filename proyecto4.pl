76f03cfedc50806dbf059fc027a2e51bad13e309
Javier Salazar <40800436+zombiewafle@users.noreply.github.com>
2021-10-26 19:41:40 -0600

    Add files via upload

---
+++ proyecto4.pl
@@ -0,0 +1,5 @@
+% Proyecto 4 de la clase de Lógica Matemática 
+% Este programa debe calcular la derivada sumas, restas, multiplicaciones, divisiones
+% , funciones polinomiales, coseno, seno, tangente, arctan, exponentes y logaritmo natural
+
+
